59daf87f1520857c90f0810c0af536aa38d6c9e2
s5707515 <s5707515@bournemouth.ac.uk>
2025-12-30 17:49:57 +0000

    Added Heart Sound

--- Assessment/Unity Project/Game Audio Project/Assets/Script/HearthPickup.cs
+++ Assessment/Unity Project/Game Audio Project/Assets/Script/HearthPickup.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class HeartPickup : MonoBehaviour
 {
+    private AudioSource playerSourceRef;
+
+    [SerializeField] private AudioClip heartPickUpSound;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerSourceRef = GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            Destroy(gameObject);
+            playerSourceRef.PlayOneShot(heartPickUpSound);
+            DestroyHeart();
         }
+    }
+
+    void DestroyHeart()
+    {
+        Destroy(gameObject);
     }
 }
