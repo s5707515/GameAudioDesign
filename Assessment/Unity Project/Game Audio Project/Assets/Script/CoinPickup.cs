23dbc57433a680b5e8c6785ad2458b0c8ba74899
s5707515 <s5707515@bournemouth.ac.uk>
2025-12-30 00:02:11 +0000

    Added Coin Pick Up sound effect

--- Assessment/Unity Project/Game Audio Project/Assets/Script/CoinPickup.cs
+++ Assessment/Unity Project/Game Audio Project/Assets/Script/CoinPickup.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    private AudioSource playerSourceRef;
+
+    [SerializeField] private AudioClip coin1;
+    [SerializeField] private AudioClip coin2;
+    [SerializeField] private AudioClip coin3;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerSourceRef = GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -19,7 +27,50 @@ public class CoinPickup : MonoBehaviour
     {
         if(other.tag == "Player")
         {
-            Destroy(gameObject);
+            playerSourceRef.PlayOneShot(PickCoinSoundToPlay());
+            DestroyCoin();
         }
+    }
+
+    AudioClip PickCoinSoundToPlay()
+    {
+        AudioClip soundToPlay = coin1;
+        
+        int soundID = Random.Range(0, 2);
+
+        switch(soundID)
+        {
+            case 0:
+                soundToPlay = coin1;
+
+                break;
+
+            case 1:
+                soundToPlay = coin2;
+
+                break;
+
+            case 2:
+
+                soundToPlay = coin3;
+
+                break;
+
+            default:
+
+                Debug.Log("Sound ID [" + soundID + "] does not exist for Coin Sounds");
+                break;
+
+          
+        }
+
+        return soundToPlay;
+
+
+    }
+
+    void DestroyCoin()
+    {
+        Destroy(gameObject);
     }
 }
